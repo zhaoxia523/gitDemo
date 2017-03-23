b7c09b70b17b46ba3082f6874a33eba47aceade8
yinzx <yinzx@hzjs.co>
2017-03-23 17:53:30 +0800

    ys

--- src/app/app.js
+++ src/app/app.js
@@ -2,18 +2,18 @@
  * Created by hzjs-pc on 2017/3/21.
  */
 'use strict';
-angular.module('app',['ui.router'])
-    .config(function($stateProvider,$urlRouterProvider){
+angular.module('app', ['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/index');
         $stateProvider
-            .state('index',{
-                url : '/index',
-                views : {
-                    '' : {
-                        templateUrl : 'app/main/main.html'
+            .state('index', {
+                url: '/index',
+                views: {
+                    '': {
+                        templateUrl: 'app/main/main.html'
                     },
-                    'main@index' : {
-                      templateUrl : 'app/yunsou/ys/ys.html'
+                    'main@index': {
+                        templateUrl: 'app/yunsou/ys/ys.html'
                     }
                 }
             })
